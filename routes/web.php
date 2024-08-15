b078e0960e1af13fc46fb31bcc053330f481f413
Isuru Bandara <isurubandara318@gmail.com>
2024-08-16 03:20:44 +0530

    middleware

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
+    'admin',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
