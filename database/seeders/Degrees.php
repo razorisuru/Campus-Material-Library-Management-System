add35ad11666b86c2fd2a7b90e5651bedf640ab7
Isuru Bandara <isurubandara318@gmail.com>
2024-10-29 16:26:24 +0530

    Update Seedr

--- database/seeders/Degrees.php
+++ database/seeders/Degrees.php
@@ -15,23 +15,48 @@ class Degrees extends Seeder
     {
         $degreeProgrammes = [
             [
+                'id' => '1',
                 'name' => 'BSc',
                 'description' => 'Bachelor of Science in Information Technology',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
+                'id' => '2',
                 'name' => 'B-Tech',
                 'description' => 'B. Tech. in Information Technology & Communication',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'name' => 'B-TECH',
-                'description' => 'btech',
+                'id' => '3',
+                'name' => 'BAGD',
+                'description' => 'BA (General) Degree',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
+            [
+                'id' => '4',
+                'name' => 'BBM',
+                'description' => 'Bachelor of Business Management (Honours) Degree',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => '5',
+                'name' => 'BABL',
+                'description' => 'BA Special Degree in Buddhist Leadership',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => '6',
+                'name' => 'PALI',
+                'description' => 'BA Special Degree in Pali Studies',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
         ];
 
         foreach ($degreeProgrammes as $programme) {
