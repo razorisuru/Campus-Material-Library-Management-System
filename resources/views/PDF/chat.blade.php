ab41ef1338b687137fac5270f68bf3c5e165dbd4
Isuru Bandara <isurubandara318@gmail.com>
2025-04-17 19:24:44 +0530

    Update chat.blade.php

--- resources/views/PDF/chat.blade.php
+++ resources/views/PDF/chat.blade.php
@@ -93,7 +93,7 @@
 
                     // Simulate AI response with AJAX
                     $.ajax({
-                        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyC-EUkLorlIfzxIDz909Mm9WkvTz5EWDF4",
+                        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={{ env('GOOGLE_API_KEY') }}",
                         type: "POST",
                         contentType: "application/json",
                         data: JSON.stringify({
